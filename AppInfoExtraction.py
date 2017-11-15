b19903a8697729f2f9ce3555ef90c0832eda3ea0
jacopomv <jac.valtorta@gmail.com>
2017-11-15 15:45:00 +0100

    Several changes and SVM function introduced

--- AppInfoExtraction.py
+++ AppInfoExtraction.py
@@ -10,13 +10,13 @@ class AppInfoExtraction:
 
     def read_list_app(self, appDir, malwareList):
 
-        malware = pd.read_csv(malwareList, sep=',')
-        for filename in os.listdir(appDir):
+        malware = pd.read_csv(malwareList, sep=',')                         #inspect the .csv file understanding which file is effectively a malware
+        for filename in os.listdir(appDir):                                 #scanning the dataset
             app = []
-            feature1=[]
-            feature2=[]
-            with open(appDir + filename) as f:
-                if (malware['sha256'].str.contains(filename).any()):
+            #feature1=[]
+            #feature2=[]
+            with open(appDir + filename) as f:                              #inpecting each file within the dataset
+                if (malware['sha256'].str.contains(filename).any()):        #It's a MALWARE!!!
                     self.malware_index.append(1)
                     #print("Malware !!! " + filename)
                 else:
@@ -24,13 +24,15 @@ class AppInfoExtraction:
                     #print("Good App")
 
 
-                limit = 4
+                #limit = 4
                 for line in f.read().splitlines():
                     arguments = line.split("::")
+                    #if arguments[0] is not None:
+                     #   app.append(arguments[1])
 
-                    #if (arguments[0] in ["permission","url","api_call","real_permission","intent"]):
-                    #    app.append(arguments[1])
-                    app.append(arguments[1])
+                    if (arguments[0] in ["permission","url","activity","api_call","real_permission","intent","feature","call"]):
+                         app.append(arguments[1])
+
 
                 self.apps.append(app)
 
